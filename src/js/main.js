7b1539096458af66a05fc23742b4777021bc0a08
Dim04ka <suhotsky.dmitry@gmail.com>
2019-09-23 17:07:57 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+window.jQuery = require('jquery');
+var $ = require('jquery');
+var jQuery = require('jquery');
+
+require('./active.js');
+
+
+
+
+
+(function($){
+
+    $(document).ready(function(){
+    });
+
+})(jQuery);
